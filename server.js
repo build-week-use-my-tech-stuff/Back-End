ebe7fe0bc37be08dfc642d3347cb39c278f1e7f4
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-16 18:29:00 -0400

    Restricted removed

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const server = express();
 const authRouter = require("./authorization/authRouter");
 const techRouter = require("./tech/techRouter");
 const usersRouter = require("./users/usersRouter");
-const { restricted } = require("./authorization/restricted");
+// const { restricted } = require("./authorization/restricted");
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-server.use("/api/tech", restricted, techRouter);
+server.use("/api/tech", techRouter);
 server.use("/api/users", usersRouter);
 
 server.get("/", async (req, res) => {
